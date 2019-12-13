7280e236ceb29d1fcd3b972ec777d4370b58b75e
basic-app <dev@basic-app.com>
2019-12-13 13:24:52 +0300

    n/a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -75,6 +75,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 
+//$routes->add('/', 'BasicApp\Site\Controllers\Page::view');
 $routes->add('admin', 'BasicApp\Admin\Controllers\Dashboard::index');
 
 /**
